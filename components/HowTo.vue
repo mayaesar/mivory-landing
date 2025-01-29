bc340b21f2206d1622138601c1c606ae49de3a40
Maya <mayaesar@gmail.com>
2025-01-29 12:51:23 -0500

    feat: how to video place holders

--- components/HowTo.vue
+++ components/HowTo.vue
@@ -14,12 +14,12 @@
           Instantly save links, articles, and media from anywhere online. With easy-to-use tools at your fingertips, capturing content on the go has never been simpler.
         </p>
       </div>
-<!--      <VideoDisplay videoUrl="/videoTest.MP4" />-->
+      <VideoDisplay videoUrl="/videoTest.MP4" />
     </div>
 
 
     <div class="md:h-dvh flex flex-col-reverse md:flex-row items-center justify-center gap-12 md:gap-24 mb-12 md:mb-0">
-<!--      <VideoDisplay videoUrl="/videoTest.MP4" />-->
+      <VideoDisplay videoUrl="/videoTest.MP4" />
       <div>
         <h1 class="text-4xl md:text-6xl font-theme font-bold mb-6">
           <span class="text-pink-700">Organize</span> Your World
@@ -40,7 +40,7 @@
           Share your collections with friends, family, or colleagues. Work together, exchange ideas, and discover new content as a team. Collaboration has never been so simple and inspiring.
         </p>
       </div>
-      <!--      <VideoDisplay videoUrl="/videoTest.MP4" />-->
+      <VideoDisplay videoUrl="/videoTest.MP4" />
     </div>
   </section>
 </template>
---
+++ components/VideoDisplay.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+type Props = {
+  videoUrl: string
+}
+const { videoUrl } = defineProps<Props>()
+</script>
+
+<template>
+  <div class="relative w-full p-4 md:p-0 md:max-w-[280px] aspect-[8/16] shrink-0 overflow-hidden">
+    <video
+        :src="videoUrl"
+        autoPlay
+        loop
+        muted
+        class="absolute top-0 left-0 w-full h-full z-0 object-cover overflow-hidden rounded-3xl p-5 md:p-4"
+    />
+    <LazyNuxtImg
+        src="/phoneMock.svg"
+        alt="phone frame"
+        fill={true}
+        class="absolute top-0 left-0 w-full h-full z-10 pointer-events-none"
+    />
+
+  </div>
+</template>
