3dbdab3a358871793abcac25ed70ce876fb56666
Maya <mayaesar@gmail.com>
2025-02-03 11:54:01 -0500

    feat: google analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: false },
-  modules: ['@nuxt/ui', '@nuxt/image'],
+  modules: ['@nuxt/ui', '@nuxt/image', 'nuxt-gtag'],
   css: ['~/assets/style/app.css'],
+  gtag: {
+    enabled: process.env.NODE_ENV === 'production',
+    id: 'G-VNNV6QT9NM'
+  }
 })
