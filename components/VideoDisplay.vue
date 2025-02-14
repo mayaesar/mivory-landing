3a6cee81cc7502f0f165dd95b7eb1607005a27f5
Maya <mayaesar@gmail.com>
2025-02-14 14:17:15 -0500

    Feat: add how to videos

--- components/VideoDisplay.vue
+++ components/VideoDisplay.vue
@@ -6,7 +6,7 @@ const { videoUrl } = defineProps<Props>()
 </script>
 
 <template>
-  <div class="relative w-full p-4 md:p-0 md:max-w-[280px] aspect-[8/16] shrink-0 overflow-hidden pointer-events-none">
+  <div class="relative w-full p-4 md:p-0 md:max-w-[280px] aspect-[9/19] shrink-0 overflow-hidden rounded-[2.5rem] border-4 border-gray-900 pointer-events-none">
     <video
         :src="videoUrl"
         :controls="false"
@@ -14,14 +14,14 @@ const { videoUrl } = defineProps<Props>()
         playsinline
         loop
         muted
-        class="absolute top-0 left-0 w-full h-full z-0 object-cover overflow-hidden rounded-3xl p-5 md:p-4"
+        class="absolute top-0 left-0 w-full h-full z-0 object-cover overflow-hidden"
     />
-    <LazyNuxtImg
-        src="/phoneMock.svg"
-        alt="phone frame"
-        fill={true}
-        class="absolute top-0 left-0 w-full h-full z-10"
-    />
+<!--    <LazyNuxtImg-->
+<!--        src="/phoneMock.svg"-->
+<!--        alt="phone frame"-->
+<!--        fill={true}-->
+<!--        class="absolute top-0 left-0 w-full h-full z-10"-->
+<!--    />-->
 
   </div>
 </template>
